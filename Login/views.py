d268bf4cca3ee77996d0c0f7fc792025e0749097
Mauricio <0253214@up.edu.mx>
2023-10-05 11:04:53 -0600

    added login form, added form to login.html and modified login endpoint from views.py

--- Login/views.py
+++ Login/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.http import HttpResponse
+from .forms import LoginForm
 
 
 # If the user goes to the root URL, redirect them to the Login page
@@ -7,10 +7,23 @@ def redirect_login(request):
     return redirect('/login/')
 
 
-def login(request):
+def login_view(request):
     """
     The Login page.
 
     This is the page that the user will see when they go to the Login URL or the root URL.
     """
-    return render(request, 'Login/login.html')
+    message = None
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            # Handle the login logic here
+            pass
+    else:
+        form = LoginForm()
+
+    context = {
+        'form': form,
+        'message': message
+    }
+    return render(request, 'Login/login.html', context)

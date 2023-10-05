8c6fe43fdb995a7800089e52ab0ffc5f98d4738d
Mauricio <0253214@up.edu.mx>
2023-10-05 01:01:09 -0600

    modified admin.py from login

--- Login/admin.py
+++ Login/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.admin.sites import NotRegistered
 
 # Register your models here.
+
+# This app is running on docker, so we need to unregister and then register the models,
+# otherwise we get an error each time we try to run the server saying that the models
+# are already registered.
+try:
+    # This is an example of how to unregister a model
+    # admin.site.unregister(User)
+    pass
+except NotRegistered:
+    pass
+
+# After unregistering the models, we can register them again
+# admin.site.register(User)

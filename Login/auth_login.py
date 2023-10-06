2e1da082e97b4e3083e643e253a0c92791f795d0
Mauricio <0253214@up.edu.mx>
2023-10-06 00:01:38 -0600

    updated requirements.txt

---
+++ Login/auth_login.py
@@ -0,0 +1,10 @@
+import requests
+
+
+def authenticate_with_peoplesoft(username, password):
+    """
+    Authenticate with Peoplesoft
+    :param username: Username
+    :param password: Password
+    :return: True if authenticated, False otherwise
+    """

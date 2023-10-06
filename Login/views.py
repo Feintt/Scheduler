f2430b069a0da143655054edb093edafec6d2d47
Mauricio <0253214@up.edu.mx>
2023-10-06 00:38:08 -0600

    finished login endpoint

--- Login/views.py
+++ Login/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
+from django.contrib.auth.models import User
 from .forms import LoginForm
 from .auth_login import authenticate_people_soft
 
@@ -14,6 +16,11 @@ def login_view(request):
 
     This is the page that the user will see when they go to the Login URL or the root URL.
     """
+
+    # If the user is already logged in, redirect them to the dashboard
+    if request.user.is_authenticated:
+        return redirect('schedule')
+
     message = None
     if request.method == 'POST':
         form = LoginForm(request.POST)
@@ -21,7 +28,18 @@ def login_view(request):
             userid = form.cleaned_data['email'].split('@')[0]
             pwd = form.cleaned_data['password']
             if authenticate_people_soft(userid, pwd):
-                message = 'Login successful!'
+                # Create user if they don't exist, otherwise just login
+                user, created = User.objects.get_or_create(username=userid)
+                if created:
+                    # If a new user was created, set an unusable password,
+                    # This is because the authentication is managed by PeopleSoft, not Django's auth
+                    user.set_unusable_password()
+                    user.save()
+
+                # Directly log the user into Django's session without re-authenticating
+                login(request, user)
+                # Redirect to a dashboard or home page after successful login
+                return redirect('schedule')
             else:
                 message = 'Login failed!'
     else:

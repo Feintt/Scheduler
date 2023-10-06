f4d6b7298a9de625aa2ae5c0666c2618414b5a21
Mauricio <0253214@up.edu.mx>
2023-10-05 20:43:52 -0600

    registered Dashboard app in settings.py

--- ScheduleMaker/settings.py
+++ ScheduleMaker/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'Login.apps.LoginConfig'
+    'Login.apps.LoginConfig',
+    'Dashboard.apps.DashboardConfig'
 ]
 
 MIDDLEWARE = [

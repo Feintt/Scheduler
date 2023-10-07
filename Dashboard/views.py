3844bbf53a7b756f369369c59df10b96452e6060
Mauricio <0253214@up.edu.mx>
2023-10-07 00:05:12 -0600

    added schedule to user session when accessing dashboard

--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-from requests.utils import dict_from_cookiejar
+from requests.utils import dict_from_cookiejar, cookiejar_from_dict
 import requests
 
 
@@ -13,7 +13,7 @@ def schedule(request):
         cookies = request.session['cookies']
         token = request.session['token']
         session = requests.Session()
-        session.cookies = requests.utils.cookiejar_from_dict(cookies)
+        session.cookies = cookiejar_from_dict(cookies)
         headers = {'Authorization': 'token ' + token}
         schedule_url = 'http://flow-api:8000/user/schedule/'
         response = session.get(schedule_url, headers=headers)
